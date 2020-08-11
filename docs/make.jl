ee04d17c5e4533031916cc3f3852fd922d0c70e0
LudwigBoess <lboess@usm.lmu.de>
2020-08-11 10:34:30 +0200

    added packagebutler support

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,14 @@ using Documenter
 using GadgetUnits
 
 makedocs(
-    sitename = "GadgetUnits",
-    format = Documenter.HTML(),
-    modules = [GadgetUnits]
+    sitename="GadgetUnits",
+    format=Documenter.HTML(),
+    modules=[GadgetUnits]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/LudwigBoess/GadgetUnits.jl.git"
+)
