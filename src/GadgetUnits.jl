f92cdc244a79703bf0a30ce59107e140ff41cb42
Tim Holy <tim.holy@gmail.com>
2023-04-24 18:26:08 -0500

    Migrate from SnoopPrecompile to PrecompileTools

--- src/GadgetUnits.jl
+++ src/GadgetUnits.jl
@@ -20,9 +20,9 @@ module GadgetUnits
            mJy_to_W
 
 
-    using SnoopPrecompile    # this is a small dependency
+    using PrecompileTools    # this is a small dependency
 
-    @precompile_setup begin
+    @setup_workload begin
         # Putting some things in `setup` can reduce the size of the
         # precompile file and potentially make loading faster.
 
@@ -35,7 +35,7 @@ module GadgetUnits
             Int32(0), Int32(0), Int32(3), 0.0f0,
             Int32[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        @precompile_all_calls begin
+        @compile_workload begin
             # all calls in this block will be precompiled, regardless of whether
             # they belong to your package or not (on Julia 1.8 and higher)
                 
